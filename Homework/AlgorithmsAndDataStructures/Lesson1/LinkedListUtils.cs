57975546632eaa9b44b6873bae7c0f69f5085565
Дмитрий <skillet123q@gmail.com>
2016-06-13 23:45:54 +0300

    Выполнено первое задание по алгоритмам и структурам данных

---
+++ Homework/AlgorithmsAndDataStructures/Lesson1/LinkedListUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsAndDataStructures.Lesson1
+{
+    public static class LinkedListUtils
+    {
+        /// <summary>
+        /// TASK: Напишите программу, которая удаляет все узлы связанного списка, находящиеся в позициях с номерами кратными пяти. 
+        /// </summary>
+        public static void RemoveMultiplesOf<T>(this LinkedList<T> linkedList, int number)
+        {
+            // Зачинаем с головы
+            var current = linkedList.First;
+            for (int i = 1; current != null; i++)
+            {
+                // Заранее узнаем следующий узел, так как при удалении текущено узла
+                // следующий узел узнать уже мы не сможем
+                var next = current.Next;
+
+                // Кратен ли итератор указаному числу в аргументе
+                if (i % number == 0)
+                    linkedList.Remove(current); // Собственно удаляем узел
+
+                current = next; // Переходим к следующему узлу
+            }
+        }
+    }
+}
--- Homework/AlgorithmsAndDataStructures/Program.cs
+++ Homework/AlgorithmsAndDataStructures/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AlgorithmsAndDataStructures.Lesson1;
 
 namespace AlgorithmsAndDataStructures
 {
@@ -10,6 +11,20 @@ namespace AlgorithmsAndDataStructures
     {
         static void Main(string[] args)
         {
+            #region Lesson1Task1
+
+            var words = new[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten" };
+            var linkedList = new LinkedList<string>(words);
+
+            linkedList.RemoveMultiplesOf(5);
+
+            Console.WriteLine("Напишите программу, которая удаляет все узлы связанного списка, находящиеся в позициях с номерами кратными пяти.");
+            foreach (var item in linkedList)
+                Console.WriteLine(item);
+
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
