815fce603888b461416c6a2027e6716cfd4657e3
Дмитрий <skillet123q@gmail.com>
2016-06-20 20:51:17 +0300

    Реализована сортировка пузырьком

---
+++ AlgorithmsAndDataStructures/Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Output<T>(T[] items)
+        {
+            Console.WriteLine();
+            foreach (var item in items)
+            {
+                Console.Write(item + " ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var items = new[] { 0, 3, 6, 4, 2, 5, 7, 9, 8 };
+            Output(items);
+
+            items.MyBubbleSort();
+            Output(items);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Sort/SortUtils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort
+{
+    public static class SortUtils
+    {
+        private static void Swap<T>(this T[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                T temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        // Метод реализует сортировку пузырьком 
+        // Author: это дано как пример в уроке
+        public static void BubbleSort(this int[] items)
+        {
+            bool swapped;
+
+            do
+            {
+                swapped = false;
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    if (items[i - 1].CompareTo(items[i]) > 0)
+                    {
+                        Swap(items, i - 1, i);
+
+                        swapped = true;
+                    }
+                }
+            }
+
+            while (swapped != false);
+        }
+
+        // TASK: Усовершенствуйте сортировку пузырьком в первом примере урока. 
+        // Подумайте, как можно уменьшить количество итераций циклов. 
+        // Author: это уже моя реализация
+        public static void MyBubbleSort<T>(this T[] items)
+            where T : IComparable<T>
+        {
+            var swapped = true;
+
+            for (int i = items.Length; swapped; i--)
+            {
+                swapped = false;
+
+                for (var j = 1; j < i; j++)
+                {
+                    if (items[j - 1].CompareTo(items[j]) > 0)
+                    {
+                        items.Swap(j - 1, j);
+                        swapped = true;
+                    }
+                }
+            }
+        }
+
+    }
+}
