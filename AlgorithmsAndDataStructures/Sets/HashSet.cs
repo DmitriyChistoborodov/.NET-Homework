cf61dc3108aabfcdd93584ca8dfcee780c492a59
Дмитрий <skillet123q@gmail.com>
2016-06-20 18:36:58 +0300

    Выполнено задание по множествам

---
+++ AlgorithmsAndDataStructures/Sets/HashSet.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sets
+{
+    //TASK: Реализуйте множество на основе массива и добавьте в него значения: “John”, “Mark”, 1, 2, 3.
+    //Author: Основной целью я себе поставил реализовать методы по работе с множеством(Объединение, разность, пересечение, и тд.)
+    //Поэтому я не наследовался от интерфейса ISet  
+    public class HashSet : ArrayList
+    {
+        public HashSet()
+        {
+
+        }
+
+        public HashSet(ICollection items) : base(items)
+        {
+        }
+
+        public HashSet Union(HashSet other)
+        {
+            var result = new HashSet(ToArray());
+
+            foreach (var item in other)
+            {
+                if (!Contains(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        public HashSet Difference(HashSet other)
+        {
+            var result = new HashSet(ToArray());
+            foreach (var item in other)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+
+        public HashSet Intersection(HashSet other)
+        {
+            var result = new HashSet();
+            var currentItems = ToArray();
+
+            foreach (var item in currentItems)
+            {
+                if (other.Contains(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+
+        public bool IsSubset(HashSet other)
+        {
+            var result = new HashSet(ToArray());
+
+            foreach (var item in other)
+            {
+                result.Remove(item);
+            }
+
+            return result.Count == 0;
+        }
+    }
+}
---
+++ AlgorithmsAndDataStructures/Sets/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool rez;
+
+            int[] array = { 1, 2, 3, 4, 5, 6 };
+
+            var set1 = new HashSet();
+            var set2 = new HashSet(array);
+
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+            set1.Add(4);
+            set1.Add(7); //Расскоментировать!
+
+            rez = set1.IsSubset(set2);
+
+            Console.WriteLine("Множество set1 является подмножеством множества set2: {0} ", rez);
+        }
+    }
+}
