5bdd526c33d8d4884c5c68bc1517e08b95b5e1de
Дмитрий <skillet123q@gmail.com>
2016-06-23 20:30:08 +0300

    Выполнено небольшое задание по HashTable

---
+++ AlgorithmsAndDataStructures/HashTableSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Изучите самостоятельно материал о классе HashTable. 
+            // Реализуйте на его основе хеш-таблицу из списка имен и фамилий ваших одноклассников. 
+            // Выведете количество человек и данные о них в консоль из хеш-таблицы.  
+
+            var students = new Hashtable()
+            {
+                {"Васильченко", "Олег" },
+                {"Борисенко", "Валентин" },
+                {"Иванов", "Александр" },
+                {"Мальченко", "Григорий" },
+            };
+
+            Console.WriteLine("Кол-во человек: {0}", students.Count);
+
+            foreach (DictionaryEntry student in students)
+            {
+                Console.WriteLine("{0} {1}", student.Key, student.Value);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
